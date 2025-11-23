a4373ada914303b985c1f85281fc1d45e71eb1e2
qzmp010 <66999686+qzmp010@users.noreply.github.com>
2025-11-23 10:09:58 -0800

    additional pages

--- index.mjs
+++ index.mjs
@@ -14,16 +14,99 @@ app.get('/', async (req, res) => {
     res.render("index", { "image": randomImage })
 });
 
+// Gets planet info using npm-solarsystem by planet name
+// Adds name property to result
+const getPlanetHelper = (name) => {
+    let planet;
+    name = name.toLowerCase();
+    switch (name) {
+        case 'mercury':
+            planet = planets.getMercury();
+            break;
+        case 'venus':
+            planet = planets.getVenus();
+            break;
+        case 'earth':
+            planet = planets.getEarth();
+            break;
+        case 'mars':
+            planet = planets.getMars();
+            break;
+        case 'jupiter':
+            planet = planets.getJupiter();
+            //fix broken image link
+            planet.image = "https://upload.wikimedia.org/wikipedia/commons/e/e2/Jupiter_OPAL_2024.png";
+            break;
+        case 'saturn':
+            planet = planets.getSaturn();
+            break;
+        case 'uranus':
+            planet = planets.getUranus();
+            break;
+        case 'neptune':
+            planet = planets.getNeptune();
+            break;
+        default:
+            break;
+    }
+    planet.name = name.charAt(0).toUpperCase() + name.slice(1);
+    console.log(planet);
+    return planet;
+}
+
+app.get('/mercury', (req, res) => {
+    const planetName = req.route.path.slice(1);
+    res.render('planet', { "planet": getPlanetHelper(planetName) });
+});
+
+app.get('/venus', (req, res) => {
+    const planetName = req.route.path.slice(1);
+    res.render('planet', { "planet": getPlanetHelper(planetName) });
+});
+
 app.get('/earth', (req, res) => {
-    let planetEarth = planets.getEarth();
-    console.log(planetEarth);
-    res.render('earth', { planetEarth });
+    const planetName = req.route.path.slice(1);
+    res.render('planet', { "planet": getPlanetHelper(planetName) });
 });
 
 app.get('/mars', (req, res) => {
-    let planetMars = planets.getMars();
-    console.log(planetMars);
-    res.render('mars', { planetMars });
+    const planetName = req.route.path.slice(1);
+    res.render('planet', { "planet": getPlanetHelper(planetName) });
+});
+
+app.get('/jupiter', (req, res) => {
+    const planetName = req.route.path.slice(1);
+    res.render('planet', { "planet": getPlanetHelper(planetName) });
+});
+
+app.get('/saturn', (req, res) => {
+    const planetName = req.route.path.slice(1);
+    res.render('planet', { "planet": getPlanetHelper(planetName) });
+});
+
+app.get('/uranus', (req, res) => {
+    const planetName = req.route.path.slice(1);
+    res.render('planet', { "planet": getPlanetHelper(planetName) });
+});
+
+app.get('/neptune', (req, res) => {
+    const planetName = req.route.path.slice(1);
+    res.render('planet', { "planet": getPlanetHelper(planetName) });
+});
+
+app.get('/nasa', async (req, res) => {
+    const apiKey = '9mUzIkhlZCZaOoMfspg7jMmwZCZ4LiRHtkgkambD';
+    const today = new Date();
+    let date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+    let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    const nasa = {
+        title: data.title,
+        explanation: data.explanation,
+        url: data.url
+    }
+    res.render('nasa', { nasa });
 });
 
 app.listen(3000, () => {
